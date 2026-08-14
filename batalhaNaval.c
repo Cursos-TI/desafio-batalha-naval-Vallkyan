f644252544c049660bd42a4a3f9c33c44a42647e
Vallkyan <andradelennon267@gmail.com>
2026-08-14 00:33:01 +0000

    Com os conhecimentos da aula no nível mestre finalmente conseguir imprimir o tabuleiro corretamente. Professor esse conhecimento deveria estarno no nivel aventureiro, bati cabeça muito tempo e só nas aulas do nível mestre tinha o assunto, precisa ajustar...

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -20,18 +20,17 @@ int main() {
     // Representação e Posicionamento do Navio Horizontal 
     tabuleiro[2][2] = 3;
     tabuleiro[2][3] = 3;
+    tabuleiro[2][4] = 3;
 
     // Representação e Posicionamento do Navio Vertical
     tabuleiro[4][5] = 3;
     tabuleiro[5][5] = 3;
     tabuleiro[6][5] = 3;
     
-
     //Saída de Dados para o Usuário
-    printf("BATALHA NAVAL");
-    for (int i = 0; i < 10; i++)
-    for (int j = 0; j < 10; j++)
-    printf("%d ", tabuleiro[i][j]);
-
-// Fechamento do aplicativo
+     for (int i = 0; i < 10; i++) {//loop para imprimir as linhas
+        for (int j = 0; j < 10; j++) {//loop para imprimir as colunas
+            printf("%d ", tabuleiro[i][j]);}//impressão do tabuleiro
+            printf("\n");} // Quebra linha ao fim de cada linha da matriz
+    // Fechamento do aplicativo
 return 0;}
